635e8c266b8185666cba2f38f31e979c945093bd
kirillsakun <kirillsakun03@gmail.com>
2022-11-29 14:31:05 +0100

    Update (Constants): Add class constants

---
+++ src/_constants/classes.js
@@ -0,0 +1,6 @@
+export const CLASSES = {
+	ACTIVE: 'js--active',
+	DISABLED: 'js--disabled',
+	OPEN: 'js--open',
+	CLOSED: 'js--closed',
+};
--- src/_constants/index.js
+++ src/_constants/index.js
@@ -1,1 +1,2 @@
 export { BREAKPOINTS } from './breakpoints.js';
+export { CLASSES } from './classes.js';
